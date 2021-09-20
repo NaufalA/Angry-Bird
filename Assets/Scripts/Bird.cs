f17867684fe881ea7b1643ff5827475c61aada33
Naufal Aulia <naufal7600@gmail.com>
2021-09-20 17:17:03 +0700

    add slingshot mechanic

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public enum BirdState
+    {
+        Idle,
+        Thrown
+    };
+
+    public GameObject parent;
+    public Rigidbody2D rigidBody;
+    public CircleCollider2D circleCollider;
+
+    private BirdState _state;
+    private float _minVelocity = 0.05f;
+    private bool _flagDestroy = false;
+
+    private void Start()
+    {
+        rigidBody.bodyType = RigidbodyType2D.Kinematic;
+        circleCollider.enabled = false;
+        _state = BirdState.Idle;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_state == BirdState.Idle && rigidBody.velocity.sqrMagnitude >= _minVelocity)
+        {
+            _state = BirdState.Thrown;
+        }
+
+        if (_state == BirdState.Thrown && rigidBody.velocity.sqrMagnitude < _minVelocity && !_flagDestroy)
+        {
+            _flagDestroy = true;
+            StartCoroutine(DestroyAfter(2f));
+        }
+    }
+
+    private IEnumerator DestroyAfter(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        Destroy(gameObject);
+    }
+
+    public void MoveTo(Vector2 target, GameObject parentObject)
+    {
+        GameObject o = gameObject;
+        o.transform.SetParent(parentObject.transform);
+        o.transform.position = target;
+    }
+
+    public void Shoot(Vector2 velocity, float distance, float speed)
+    {
+        circleCollider.enabled = true;
+        rigidBody.bodyType = RigidbodyType2D.Dynamic;
+        rigidBody.velocity = velocity * speed * distance;
+    }
+}
---
+++ Assets/Scripts/SlingShooter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlingShooter : MonoBehaviour
+{
+    public CircleCollider2D collider;
+    private Vector2 _startPos;
+
+    [SerializeField] private float radius = 0.75f;
+    [SerializeField] private float throwSpeed = 30f;
+
+    private Bird _bird;
+    private void Start()
+    {
+        _startPos = transform.position;
+    }
+
+    private void OnMouseUp()
+    {
+        collider.enabled = false;
+        Vector2 velocity = _startPos - (Vector2) transform.position;
+        float distance = Vector2.Distance(_startPos, transform.position);
+        
+        _bird.Shoot(velocity, distance, throwSpeed);
+
+        gameObject.transform.position = _startPos;
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector2 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = point - _startPos;
+        if (direction.sqrMagnitude > radius)
+        {
+            direction = direction.normalized * radius;
+        }
+
+        transform.position = _startPos + direction;
+    }
+
+    public void InstantiateBird(Bird bird)
+    {
+        _bird = bird;
+        _bird.MoveTo(transform.position, gameObject);
+        collider.enabled = true;
+    }
+}
