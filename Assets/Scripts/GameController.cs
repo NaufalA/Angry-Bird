fb65031b923c46d0ff6aaf19467172d984c475f9
Naufal Aulia <naufal7600@gmail.com>
2021-09-20 18:10:38 +0700

    add enemies and obstacles

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,6 +7,9 @@ public class GameController : MonoBehaviour
 {
     public SlingShooter slingShooter;
     public List<Bird> birds;
+    public List<Enemy> enemies;
+
+    private bool _isGameEnded = false;
 
     private void Start()
     {
@@ -15,6 +18,11 @@ public class GameController : MonoBehaviour
         for (int i = 0; i < birds.Count; i++)
         {
             birds[i].OnBirdDestroyed += ChangeBird;
+        }
+
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].OnEnemyDestroyed += CheckGameEnd;
         }
     }
 
@@ -25,6 +33,23 @@ public class GameController : MonoBehaviour
         if (birds.Count > 0)
         {
             slingShooter.InstantiateBird(birds[0]);
+        }
+    }
+
+    public void CheckGameEnd(GameObject destroyedEnemy)
+    {
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            if (enemies[i].gameObject == destroyedEnemy)
+            {
+                enemies.RemoveAt(i);
+                break;
+            }
+
+            if (enemies.Count == 0)
+            {
+                _isGameEnded = true;
+            }
         }
     }
 }
